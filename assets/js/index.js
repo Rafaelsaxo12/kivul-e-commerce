1f1748133f8007b32791af685d72c17a00af7ab6
Rafael Pérez <rafaelsaxo12@gmail.com>
2024-12-05 22:30:18 -0400

    footer advances

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,24 +1,27 @@
-    let currentSlide = 0;
-    const slides = document.querySelectorAll('.slide');
 
-    function showSlide(index) {
-        slides.forEach((slide, i) => {
-            slide.classList.remove('active', 'inactive');
-            if (i === index) {
-                slide.classList.add('active');
-            } else {
-                slide.classList.add('inactive');
-            }
-        });
-    }
+    document.addEventListener('DOMContentLoaded', (event) => {
+        let currentSlide = 0;
+        const slides = document.querySelectorAll('.slide');
 
-    function nextSlide() {
-        currentSlide = (currentSlide + 1) % slides.length;
+        function showSlide(index) {
+            slides.forEach((slide, i) => {
+                slide.classList.remove('active', 'inactive');
+                if (i === index) {
+                    slide.classList.add('active');
+                } else {
+                    slide.classList.add('inactive');
+                }
+            });
+        }
+
+        function nextSlide() {
+            currentSlide = (currentSlide + 1) % slides.length;
+            showSlide(currentSlide);
+        }
+
+        setInterval(nextSlide, 5000); // Cambia de slide cada 5 segundos
+
+        // Inicializa la primera slide
         showSlide(currentSlide);
-    }
-
-    setInterval(nextSlide, 5000); // Cambia de slide cada 5 segundos
-
-    // Inicializa la primera slide
-    showSlide(currentSlide);
+    });
 
